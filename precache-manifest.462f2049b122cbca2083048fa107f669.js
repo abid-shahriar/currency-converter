4b28643b1330e54f02c5d10680c7c9366eeb9e86
abid7 <abidshahriar7@gmail.com>
2020-08-24 21:21:16 +0600

    Updates

---
+++ precache-manifest.462f2049b122cbca2083048fa107f669.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c545d2f813449f940290dba8bb7db5af",
+    "url": "/currency-converter/index.html"
+  },
+  {
+    "revision": "33b3f6f16aa37460dadc",
+    "url": "/currency-converter/static/css/main.1c95f0e2.chunk.css"
+  },
+  {
+    "revision": "66950ece9b1363e81df3",
+    "url": "/currency-converter/static/js/2.75623caa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/currency-converter/static/js/2.75623caa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33b3f6f16aa37460dadc",
+    "url": "/currency-converter/static/js/main.4b1c095f.chunk.js"
+  },
+  {
+    "revision": "1dc89b6acf8ab79f061a",
+    "url": "/currency-converter/static/js/runtime-main.eecf4b94.js"
+  }
+]);
